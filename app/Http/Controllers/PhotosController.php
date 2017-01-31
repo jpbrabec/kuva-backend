26064494f67c6e48cdf26de9cbe48bba7683237b
Jacob Brabec <jbrabec@purdue.edu>
2017-01-31 14:20:43 -0500

    Added GetPhotosByDistance method

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -26,8 +26,10 @@ class PhotosController extends Controller
      * @return Response
      */
     public function create(Request $request)
-    {    
+    {
         $validator = \Validator::make($request->all(), [
+            'lat' => 'required',
+            'lng' => 'required',
             'photo' => 'required|image',
         ]);
         if ($validator->fails()) {
@@ -69,11 +71,23 @@ class PhotosController extends Controller
         $like->photo_id = $photo->id;
         $like->user_id = Auth::user()->id;
         $like->save();
-        return ['message' => 'success'];        
+        return ['message' => 'success'];
     }
 
-    public function feed() {
-        var_dump($this->getByDistance(40.423,86.921,50));
+    public function feed(Request $request) {
+      $validator = Validator::make($request->all(), [
+          'lat' => 'required|numeric',
+          'lng' => 'required|numeric',
+      ]);
+      if ($validator->fails()) {
+          return $validator->errors()->all();
+      }
+
+      //TODO- Validate lat/lng more carefully since they go into a raw query
+      $photosInRange = Photo::getByDistance($request->lat, $request->lng, 200); //TODO- What radius makes sense
+
+      //TODO- Filter these by popularity/time/etc
+      return $photosInRange;
     }
 
     public function userPhotos() {
@@ -81,17 +95,4 @@ class PhotosController extends Controller
         return $photos;
     }
 
-    public static function getByDistance($latitude, $longitude, $radius) {
-        //http://stackoverflow.com/questions/34010916/how-to-show-stores-with-nearest-long-lat-values-using-search-function-laravel
-        // $photos = Photo::selectRaw('( 6371 * acos( cos( radians(?) ) *
-        //                        cos( radians( lat ) )
-        //                        * cos( radians( lng ) - radians(?)
-        //                        ) + sin( radians(?) ) *
-        //                        sin( radians( lat ) ) )
-        //                      ) AS distance', [$latitude, $longitude, $latitude])
-        //     ->whereRaw("distance < ?", [$radius])
-        //     ->get();
-        // $results = DB::select(DB::raw('SELECT id, ( 3959 * acos( cos( radians(' . $latitude . ') ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(' . $longitude . ') ) + sin( radians(' . $latitude .') ) * sin( radians(lat) ) ) ) AS distance FROM photos WHERE distance < ' . $radius . ' ORDER BY distance') );
-        return $photos;
-    }
 }
--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Photo extends Model
 {
 	protected $hidden = ['deleted_at'];
+
+	//Raw query to find distance
+	public static function getByDistance($lat, $lng,$distance) {
+		$results = DB::select(DB::raw('SELECT id, ( 3959 * acos( cos( radians(' . $lat . ') ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(' . $lng . ') ) + sin( radians(' . $lat .') ) * sin( radians(lat) ) ) ) AS distance FROM photos HAVING distance < ' . $distance . ' ORDER BY distance') );
+ 		return $results;
+	}
 }
