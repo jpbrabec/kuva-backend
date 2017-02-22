2a9e245af3ec253e18196ecb1bca4cb21efb9a36
Jacob Brabec <jbrabec@purdue.edu>
2017-02-22 15:01:55 -0500

    Email template fix

--- resources/views/emails/report.blade.php
+++ resources/views/emails/report.blade.php
@@ -1,9 +1,13 @@
 A photo on Kuva has been reported.
 
 REASON: {{$reportMessage}}
+
 PHOTO: kuva.jakebrabec.me/storage/uploads/{{$photo->id}}.jpg
+
 CAPTION: {{$photo->caption}}
 
+
+---------------
 
 If you want to delete this photo, click the link below:
 
